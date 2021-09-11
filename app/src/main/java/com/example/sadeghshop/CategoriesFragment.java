a0f692182f124325a5c6db52ee52f75d88c46b95
sadegh15khedry <sadegh15khedry@gmail.com>
2021-09-11 11:22:56 +0430

    started working on categories fragment

--- app/src/main/java/com/example/sadeghshop/CategoriesFragment.java
+++ app/src/main/java/com/example/sadeghshop/CategoriesFragment.java
@@ -7,11 +7,18 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class CategoriesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        ArrayList<CategoriesItem> categoriesItems = new ArrayList<>();
+        categoriesItems.add(new CategoriesItem(R.drawable.homer_simpson,"هومر","سیمسون"));
+        categoriesItems.add(new CategoriesItem(R.drawable.homer_simpson,"هومر","سیمسون"));
+        categoriesItems.add(new CategoriesItem(R.drawable.homer_simpson,"هومر","سیمسون"));
         return inflater.inflate(R.layout.fragment_categories,container,false);
     }
 }
